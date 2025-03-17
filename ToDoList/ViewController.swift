273f57161d381b8ada65d1b433a2d5f4a9561bd0
2Gpb <Pvtyukhmenev@gmail.com>
2025-03-17 16:01:38 +0300

    initial commit

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Peter on 17.03.2025.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
